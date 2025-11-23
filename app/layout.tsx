5c568c6a1b74591963272b800cfc6626e542637a
Vaibhav Kharate <vaibhav.s.kharate@gmail.com>
2025-11-23 21:41:03 +0530

    Refactor layout component: streamline head and body structure, ensuring proper favicon links and meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicons/favicon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/favicons/favicon-64x64.png" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="msapplication-TileImage" content="/favicons/web-app-manifest-192x192.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${inter.className} bg-slate-950 text-gray-200 antialiased relative`}
       >
@@ -32,14 +45,14 @@ export default function RootLayout({
         
         {/* Page Wrapper */}
         <div className="relative z-10 min-h-screen flex flex-col">
-          {/* NAVBAR */}
-          { <Navigation />}
+        {/* NAVBAR */}
+        <Navigation />
 
-          {/* MAIN CONTENT */}
-          <main className="flex-1 w-full">{children}</main>
+        {/* MAIN CONTENT */}
+        <main className="flex-1 w-full">{children}</main>
 
-          {/* FOOTER */}
-          { <Footer />}
+        {/* FOOTER */}
+        <Footer />
         </div>
       </body>
     </html>
